afe5572f9f30cdf3da3f12a2a178936f42b98774
Ivan Macalak <macalak@itexperts.sk>
2019-09-26 13:26:53 +0200

    pridany admin server aj actuatory

---
+++ serviceadmin/src/main/java/com/gopas/serviceadmin/ServiceadminApplication.java
@@ -0,0 +1,13 @@
+package com.gopas.serviceadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceadminApplication.class, args);
+	}
+
+}
